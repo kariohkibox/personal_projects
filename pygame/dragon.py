5b32672eec2c28d0c8f1a8fcded2de141a9c9433
kariohki <55408673+kariohkibox@users.noreply.github.com>
2019-09-22 17:11:46 -0400

    moved dragon class to its own file

---
+++ pygame/dragon.py
@@ -0,0 +1,26 @@
+import pygame
+
+width = 1024
+height = 576
+
+class Dragon(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale((pygame.image.load('dragon_2.png')), (150, 80))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 100
+        self.rect.bottom = height / 2
+        self.speedy = 0
+
+    def update(self):
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_UP]:
+            self.speedy = -5
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 5
+        self.rect.y += self.speedy
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > height:
+            self.rect.bottom = height
