3c519141d5baeb2541eb80aecc9262873f30697f
kariohki <55408673+kariohkibox@users.noreply.github.com>
2019-09-22 23:27:51 -0400

    added file

---
+++ pygame/fireball.py
@@ -0,0 +1,21 @@
+import pygame
+
+width = 1024
+height = 576
+
+yellow = (210, 210, 0)
+
+
+class Fireball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(yellow)
+        self.rect = self.image.get_rect()
+        self.speedx = 10
+
+    def update(self):
+        self.rect.x += self.speedx
+        # kill if it moves off the right of the screen
+        if self.rect.left > width:
+            self.kill()
